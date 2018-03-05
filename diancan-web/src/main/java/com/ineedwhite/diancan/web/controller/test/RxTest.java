af55f41fa091260204a9c4f9246e7403e0e172df
Ruan-Xin <415131215@qq.com>
2018-03-05 20:41:09 +0800

    mysql

--- diancan-web/src/main/java/com/ineedwhite/diancan/web/controller/test/RxTest.java
+++ diancan-web/src/main/java/com/ineedwhite/diancan/web/controller/test/RxTest.java
@@ -1,8 +1,14 @@
 package com.ineedwhite.diancan.web.controller.test;
 
+import com.ineedwhite.diancan.dao.dao.TestDao;
+import com.ineedwhite.diancan.dao.domain.TestDo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @author ruanxin
@@ -12,10 +18,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/test")
 public class RxTest {
+    @Autowired
+    TestDao testDao;
 
     @ResponseBody
     @RequestMapping("/test")
     public String version() {
         return "20180305";
+    }
+
+    @ResponseBody
+    @RequestMapping("/testMysql")
+    public String testMysql() {
+        List<TestDo> list =  testDao.queryAll();
+        return list.get(0).getTest_author();
     }
 }
